e4dfd29331e8ddb2e7c62a4615c933c90a74cfef
Padraig <38268519+giardap@users.noreply.github.com>
2025-07-16 15:24:38 -0400

    Initial Commit

---
+++ DonkToolTests/DonkToolTests.swift
@@ -0,0 +1,17 @@
+//
+//  DonkToolTests.swift
+//  DonkToolTests
+//
+//  Created by Padraig Marks on 7/16/25.
+//
+
+import Testing
+@testable import DonkTool
+
+struct DonkToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
